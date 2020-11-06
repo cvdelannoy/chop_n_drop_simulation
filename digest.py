270f028570cce492b5693a72a003386d694a92b0
Carlos de Lannoy <cvdelannoy@gmail.com>
2020-11-06 17:17:34 +0100

    account for charge and dynamic range

--- digest.py
+++ digest.py
@@ -1,15 +1,20 @@
 import argparse, os, sys, re, pickle
 from os.path import basename, splitext
 import pandas as pd
+import numpy as np
 from helpers import parse_output_dir, parse_input_dir, create_digest_re, digest
 from Bio.SeqIO import FastaIO
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
-
+from Bio.SeqUtils.IsoelectricPoint import IsoelectricPoint
+from math import inf
 
 parser = argparse.ArgumentParser(description='In silico digest sequences in fasta files, store their'
                                              'chop-n-drop fingerprints.')
 parser.add_argument('--in-dir',type=str, required=True)
 parser.add_argument('--out-pkl', type=str, required=True)
+parser.add_argument('--dynamic-range', type=float, nargs=2, default=[0.0, inf])
+parser.add_argument('--min-charge', type=float, default=-100)
+parser.add_argument('--ph', type=float, default=7.0)
 parser.add_argument('--enzyme', type=str, default='trypsin',
                     help='Define which enzyme to use for digestion [default: trypsin]') # todo add options
 args = parser.parse_args()
@@ -27,11 +32,20 @@ for fai, fa_fn in enumerate(fasta_list):
     with open(fa_fn) as fh:
         for rec in FastaIO.SimpleFastaParser(fh): header, seq = rec
     if 'X' in seq: continue
-    sseq_list = digest_re.findall(seq)
-    mw_list = [ProteinAnalysis(ss).molecular_weight() for ss in sseq_list]
-    nb_seqs = len(sseq_list)
-    entry_dict = {f'ss{i}': ss for i, ss in enumerate(mw_list)}
-    entry_dict['mw'] = sum(mw_list)
+    sseq_list = np.array(digest_re.findall(seq))
+    sseq_df = pd.DataFrame({'seq': sseq_list,
+                            'mw': [ProteinAnalysis(ss).molecular_weight() for ss in sseq_list],
+                            'charge': [IsoelectricPoint(ss).charge_at_pH(args.ph) for ss in sseq_list]})
+    tot_weight = sseq_df.mw.sum()
+
+    # Remove peptides if too light or too negatively charged, set max weight
+    sseq_df.query(f'mw > {args.dynamic_range[0]} and charge > {args.min_charge}', inplace=True)
+    sseq_df.loc[sseq_df.mw > args.dynamic_range[1], 'mw'] = args.dynamic_range[1]
+
+
+    nb_seqs = len(sseq_df)
+    entry_dict = {f'ss{i}': ss for i, ss in enumerate(sseq_df.mw)}
+    entry_dict['mw'] = tot_weight
     entry_dict['seq_len'] = len(seq)
     if nb_seqs in mw_dict:
         mw_dict[nb_seqs].append(pd.Series(entry_dict, name=fa_id))
--- run_sim.py
+++ run_sim.py
@@ -3,7 +3,7 @@ import argparse
 from jinja2 import Template
 from helpers import parse_output_dir
 import snakemake as sm
-
+from math import inf
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
@@ -11,9 +11,17 @@ parser = argparse.ArgumentParser(description='Run full chop n drop simulation.')
 parser.add_argument('--out-dir', type=str, required=True)
 parser.add_argument('--fasta-dir', type=str, required=False)
 parser.add_argument('--enzyme', type=str, default='trypsin',
-                    help='Enzyme with which to digest.')
-parser.add_argument('--res-range', nargs=2, type=float, default=[1000, 4000],
-                    help='Weight resolution range to visit with 20 increments. [default: 1000 4000]')
+                    help='Enzyme with which to digest. [default: trypsin]')
+parser.add_argument('--res-range', nargs=2, type=float, default=[1, 21],
+                    help='Weight resolution range to visit with 20 increments. [default: 1 21]')
+parser.add_argument('--dynamic-range', type=float, nargs=2, default=[0.0, 10E99],
+                    help='Lower and upper limit between which resolution is valid. Below lower limit,'
+                         'particle is not registered, above higher limit it is registered at maximum weight.'
+                         '[default: 0 10E99]')
+parser.add_argument('--min-charge', type=float, default=-100,
+                    help='Minimum charge in units at which fragments are still passing the pore. [default: -100]')
+parser.add_argument('--ph', type=float, default=7.0,
+                    help='pH at which charge of fragment is calculated. [default: 7.0]')
 parser.add_argument('--scrambled', action='store_true',
                     help='Ignore order of fragments in constructing fingerprint.')
 parser.add_argument('--cores', type=int, default=4,
@@ -29,6 +37,8 @@ sf_txt = Template(sf_template_txt, ).render(
     enzyme=args.enzyme,
     fasta_dir=args.fasta_dir,
     scrambled=args.scrambled,
+    min_mw=args.dynamic_range[0], max_mw=args.dynamic_range[1],
+    min_charge=args.min_charge, ph=args.ph,
     min_res=args.res_range[0], max_res=args.res_range[1]
 )
 
